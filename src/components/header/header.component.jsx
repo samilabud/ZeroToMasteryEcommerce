4573a98f6124252bdf621ee6d1ade362b470942d
samilabud <samilabud@gmail.com>
2021-01-19 17:14:10 -0400

    Uploading cart events and images

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
+
 import {auth} from '../../fireabase/firebase.utils';
 import './header.styles.scss';
 import { connect } from 'react-redux';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -22,14 +25,15 @@ const Header = ({currentUser}) => (
                     <div className='option' onClick={()=>auth.signOut()}>SALIR</div>
                     :
                     <Link className='option' to='/signin'>AUTENTICARSE</Link>
-
-
                 }
+                <CartIcon />
         </div>
+        { hidden?null:<CartDropDown /> }
+       
     </div>
 )
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user:{currentUser},cart:{hidden}}) => ({
+    currentUser, hidden
 })
 
 export default connect(mapStateToProps)(Header);
