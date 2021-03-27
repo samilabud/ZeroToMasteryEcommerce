9e1aadb58f7fba5f2d32bdd8a17c3471941a1aad
samilabud <samilabud@gmail.com>
2021-03-27 18:58:16 -0400

    adding cart items remove, clear and add from checkout and cart component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -14,4 +14,28 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 	
 	return [...cartItems, {...cartItemToAdd, quantity: 1}];
 };
+
+export const removeCartItem = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    )
+
+    if(existingCartItem.quantity===1){
+        return cartItems.filter(ci=>ci.id!==cartItemToRemove.id);
+    }
+    return cartItems.map(
+        cartItem=>
+        cartItem.id===cartItemToRemove.id?
+        {...cartItem, quantity:cartItem.quantity-1}
+        : cartItem
+    )
+}
+
+export const clearCartItemFromCart = (cartItems, cartItemToRemove) => {
+    const cartItemsCleared = cartItems.filter(
+        cartItem=> cartItem.id!==cartItemToRemove.id
+    )
+    return cartItemsCleared;
+}
+
 export default addItemToCart;
