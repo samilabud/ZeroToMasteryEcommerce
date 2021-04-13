c42ded648ac3da86575df82d47d1bb00b88d9658
samilabud <samilabud@gmail.com>
2021-04-12 22:50:52 -0400

    fixing image

--- src/components/stripe-button/stripe-button-component.jsx
+++ src/components/stripe-button/stripe-button-component.jsx
@@ -4,7 +4,7 @@ import StripeCheckout from 'react-stripe-checkout';
 const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
     const publishablekey = 'pk_test_51IfAwgF7VDSpoZmOV8VtWIwE4hJOLce3ch0DPT5DkwR8Gm0g8DWhqqxGmI3qkSywYrnGEbdIbhaNNXpbTTpzWgGO00J2aOGAhi';
-    const secretkey = 'sk_test_51IfAwgF7VDSpoZmO90Kbmy03DFqIOQxE6r6niC190Z79kp94GJIILU8bGTjlFlOIN9TD4LTxdaoFCeQ3HzkPLXiw00sifdGSKj';
+    //const secretkey = 'sk_test_51IfAwgF7VDSpoZmO90Kbmy03DFqIOQxE6r6niC190Z79kp94GJIILU8bGTjlFlOIN9TD4LTxdaoFCeQ3HzkPLXiw00sifdGSKj';
     const onToken = token => {
         console.log(token);
         alert('Pago satisfactorio');
@@ -12,10 +12,10 @@ const StripeCheckoutButton = ({price}) => {
     return (
         <StripeCheckout
             label='Paga ahora'
-            name='SamECommerce'
+            name='SamCrownCommerce'
             billingAddress
             shippingAddress
-            image='https://sendeyo.com/en/f3eb2117da'
+            image='https://www.flaticon.es/svg/vstatic/svg/91/91202.svg?token=exp=1618283036~hmac=e99a82c8578aee272351bec471b3b27d'
             description={`Your total is $${price}`}
             amount={priceForStripe}
             panelLabel='Pague ahora'
