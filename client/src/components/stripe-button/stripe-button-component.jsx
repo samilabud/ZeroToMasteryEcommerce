e88f1f38cf61d9c029ed36bfe48d67c4e29df8f6
samilabud <samilabud@gmail.com>
2021-07-13 23:31:51 -0400

    remove gitignore files

--- client/src/components/stripe-button/stripe-button-component.jsx
+++ client/src/components/stripe-button/stripe-button-component.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
     const publishablekey = 'pk_test_51IfAwgF7VDSpoZmOV8VtWIwE4hJOLce3ch0DPT5DkwR8Gm0g8DWhqqxGmI3qkSywYrnGEbdIbhaNNXpbTTpzWgGO00J2aOGAhi';
     //const secretkey = 'sk_test_51IfAwgF7VDSpoZmO90Kbmy03DFqIOQxE6r6niC190Z79kp94GJIILU8bGTjlFlOIN9TD4LTxdaoFCeQ3HzkPLXiw00sifdGSKj';
     const onToken = token => {
-        console.log(token);
-        alert('Pago satisfactorio');
+        axios({
+            url: 'payment',
+            method: 'post',
+            data:{
+                amount: priceForStripe,
+                token
+            }
+        }).then(response=> {
+            alert('Payment Successful')
+        }).catch(error=>{
+            console.log('Payment error: ' + JSON.parse(error));
+            alert("there was an issue with you payment. Please sure you use the provide credit card");
+        })
     }
     return (
         <StripeCheckout
---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+//const cors = require('cors'); //ya no se necesita
+//const bodyParser = require('body-parser');  Ya no se usa, ahora se usa el mismo express, ver debajo
+const path = require('path');
+
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
+const stripe = require('stripe')(process.env.STRYPE_SECRET_KEY);
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended:true})) deprecated, usar como se indica debajo
+
+app.use(express.urlencoded({extended: false})); //parsear el request por post y html por form url encoded
+app.use(express.json()); //parsear el request por json
+
+//app.use(cors()); //ya no se necesita
+
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname,'cliente/build')));
+
+    app.get('*', function(req, res){
+        res.sendFile(path.join(__dirname,'client/build','index.html'));
+    })
+}
+
+app.listen(port, error=>{
+    if(error){
+        throw error;
+    }
+    console.log('Server running on port ' + port);
+
+});
+
+app.post('/payment',function(req,res){
+    const body = {
+        source: req.body.token.id,
+        amount: req.body.amount,
+        currency: 'usd'
+    }
+
+    stripe.charges.create(body, (stripeErr, stripeRes)=>{
+        if(stripeErr){
+            res.status(500).send({error: stripeErr});
+        }else{
+            res.status(200).send({success:stripeRes});
+        }
+
+    });
+
+})
