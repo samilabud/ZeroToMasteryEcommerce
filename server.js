064d0fc1e836f93ccab0de5e55e6ab3ddbf3208d
samilabud <samilabud@gmail.com>
2021-07-29 15:52:42 -0400

    including gzip optimization

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 //const cors = require('cors'); //ya no se necesita
 //const bodyParser = require('body-parser');  Ya no se usa, ahora se usa el mismo express, ver debajo
 const path = require('path');
+const compression = require('compression');
 
 //usar el .env si no estamos en produccion, de lo contrario setear las variables en produccion tipo:
      //heroku config:set STRYPE_SECRET_KEY=1234
@@ -16,7 +17,7 @@ const port = process.env.PORT || 5000;
 
 //app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({extended:true})) deprecated, usar como se indica debajo
-
+app.use(compression());
 app.use(express.urlencoded({extended: false})); //parsear el request por post y html por form url encoded
 app.use(express.json()); //parsear el request por json
 
