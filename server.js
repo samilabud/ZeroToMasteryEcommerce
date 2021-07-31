34c3a72e77b66dcdfd04a0185234ca90e0b83073
samilabud <samilabud@gmail.com>
2021-07-31 17:34:05 -0400

    Adding services workers, PWA

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 //const bodyParser = require('body-parser');  Ya no se usa, ahora se usa el mismo express, ver debajo
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 //usar el .env si no estamos en produccion, de lo contrario setear las variables en produccion tipo:
      //heroku config:set STRYPE_SECRET_KEY=1234
@@ -20,6 +21,7 @@ const port = process.env.PORT || 5000;
 app.use(compression());
 app.use(express.urlencoded({extended: false})); //parsear el request por post y html por form url encoded
 app.use(express.json()); //parsear el request por json
+app.use(enforce.HTTPS({trustProtoHeader:true}));
 
 //app.use(cors()); //ya no se necesita
 
