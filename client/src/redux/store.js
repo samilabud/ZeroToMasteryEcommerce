a305e1e31b6fda7faf39b2027d8a591398520c5a
Samil Abud <samilabud@gmail.com>
2024-03-07 17:46:12 -0400

    Fixed removed logger on production

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,17 +1,17 @@
-import {createStore, applyMiddleware} from 'redux';
-import logger from 'redux-logger';
-import  { persistStore } from 'redux-persist';
-import createSagaMiddleWare from 'redux-saga';
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import { persistStore } from "redux-persist";
+import createSagaMiddleWare from "redux-saga";
 
-import rootReducer from './root-reducer';
-import rootSaga from './root-saga';
+import rootReducer from "./root-reducer";
+import rootSaga from "./root-saga";
 
 const sagaMiddleWare = createSagaMiddleWare();
 
 const middlewares = [sagaMiddleWare];
 
-if(process.env.NODE_ENV==='development'){
-    middlewares.push(logger);
+if (process.env.NODE_ENV !== "production") {
+  middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
